51cfbdb1ddfcb04744393ef5f2e07e4be6b3e1dd
danil <danil@fucking.moscow>
2024-12-04 21:52:47 +0300

    script for export sql schema

---
+++ export_db_schema.py
@@ -0,0 +1,15 @@
+from sqlalchemy import MetaData
+from sqlalchemy.sql.ddl import CreateTable
+
+from app import db, app as flask_app
+
+with flask_app.app_context():
+    metadata = MetaData()
+
+    # Отражаем существующую схему БД
+    metadata.reflect(bind=db.engine)
+
+    with open('schema.sql', 'w') as f:
+        for table in metadata.sorted_tables:
+            create_table = str(CreateTable(table)).strip() + ';\n\n'
+            f.write(create_table)
